9efbf3ef2d5c8b0978688fe5afca9c9d0e6ac12d
Md-raihan31 <126178690+Md-raihan31@users.noreply.github.com>
2023-10-05 16:49:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+<script type="module">
+  import { Toast } from 'bootstrap.esm.min.js'
+
+  Array.from(document.querySelectorAll('.toast'))
+    .forEach(toastNode => new Toast(toastNode))
+</script>
