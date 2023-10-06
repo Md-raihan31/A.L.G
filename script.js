3cc5cc9c18c8de60ae947c964becac2d8bcb8269
Md-raihan31 <126178690+Md-raihan31@users.noreply.github.com>
2023-10-06 20:13:20 +0600

    script.js

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const hostname = '127.0.0.1';
 const port = 6969;
 const home = fs.readFileSync(./index.html)
 const server = http.createserver((req, res)=>{
-console.log(req.url)
+console.log(req.url);
+url = req.url;
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
 res.end(‘Hello  world’);
