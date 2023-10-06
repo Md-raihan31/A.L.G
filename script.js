ed47a7d96c54658096a9b8b7c0a7b64c90ce1476
Md-raihan31 <126178690+Md-raihan31@users.noreply.github.com>
2023-10-06 18:27:45 +0600

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+
 const http = require('http');
+
 const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 6969;
+const server = http.createServer((req, res)=>{
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.end(‘Hello World’);
