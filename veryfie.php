958dcceaca89c7765415cde56db29119b7f90567
Md-raihan31 <126178690+Md-raihan31@users.noreply.github.com>
2024-01-02 16:19:07 +0600

    veryfie.php

--- veryfie.php
+++ veryfie.php
@@ -1,12 +1,1 @@
-<?php
-$otp=rand(00000,99999);
-?>
 
-OTP VERYFIE
-
-<form action="otp.php" method="post">
-  <input type="email" name="email" placeholder="enter new email" required>
-  <input type="hidden" name="otp" value="<?php echo $otp; ?>">
-  <button type="submit">veryfie</button>
-
-</form>
